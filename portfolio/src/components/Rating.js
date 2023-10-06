9659dc235c40b88b83477b9d1b0c9fac123300b7
yashdesai010 <yashdesai010@gmail.com>
2023-10-06 17:02:41 +0530

    updated

--- portfolio/src/components/Rating.js
+++ portfolio/src/components/Rating.js
@@ -13,7 +13,7 @@ function Rating() {
     e.preventDefault();
     toast.success("Successfully Submitted");
     try {
-      const response = await fetch('http://localhost:5000/api/feedback', {
+      const response = await fetch('https://software-developer-portfolio.onrender.com/api/feedback', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
